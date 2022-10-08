23056afac582434706e00ff2478d0b9f87f758a7
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-08 23:20:16 +0100

    test

--- primes.py
+++ primes.py
@@ -2,20 +2,14 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    list = []
     if number_of_primes > 1:
         for num in range(2, int(number_of_primes**0.5) + 1):
-            if number_of_primes % num == 0:
-                return False
-        return True
-    return False
-
-def main():
-    primers = []
-    for i in range(100, 301):
-        if primes(i):
-            primers.append(i)
-    print(primers)
-
-main()
+            if number_of_primes % num != 0:
+                for j in range(2,number_of_primes):
+                    list.append(num)
+            return list
+    else:
+        list.append[2]
 
 
