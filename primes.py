d5d45d23c8ddda84796592de518b49fc661ac813
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-08 22:11:23 +0100

    solved

--- primes.py
+++ primes.py
@@ -7,6 +7,6 @@ def primes(number_of_primes):
         list.append(2)
     else:
         for i in range(1, number_of_primes+1):
-            if i/1 ==i and i / i ==1:
+            if i/1 ==i and i / i ==1 and i%2 != 0:
                 list.append(i)
     return list
