7dcf9fde045b6d399cac0e12244f1a45f6b189d2
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-08 22:04:30 +0100

    test3

--- primes.py
+++ primes.py
@@ -3,8 +3,8 @@
 
 def primes(number_of_primes):
     list = []
-    if number_of_primes ==0:
-        return []
+    if number_of_primes ==1:
+        list.append(2)
     else:
         for i in range(number_of_primes):
             if i/1 ==i and i / i ==1:
