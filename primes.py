824c6ea140558e6203bc55338e7797bba98b5130
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-08 23:38:45 +0100

    test

--- primes.py
+++ primes.py
@@ -3,11 +3,21 @@
 
 def primes(number_of_primes):
     list = []
-    if number_of_primes > 1:
-        for i in range (2,number_of_primes+1):
-            for j in range(2,int(i**0.5)+1):
-                if i%j==0:
+    count = 1
+    while True:
+        if number_of_primes <= 0:
+            print(".")
+            break
+        if count > 1:
+            for i in range(2,count):
+                if (count % i) == 0:
                     break
             else:
-                list.append(i)
-        return list
+                list.append(count)
+        if (len(list) == number_of_primes):
+            print(list)
+            break
+        else:
+            count += 1
+            continue
+
