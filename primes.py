27fa4919099d1201729f1fca7943a57c7bc0541d
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-08 22:20:25 +0100

    test

--- primes.py
+++ primes.py
@@ -6,7 +6,7 @@ def primes(number_of_primes):
     if number_of_primes == 1:
         list.append(2)
     else:
-        for i in range(2, number_of_primes+1):
-            if i/1 ==i and i / i ==1 and (i%2 != 0 or i == 2):
+        for i in range(2, number_of_primes+2):
+            if i/1 ==i and i / i ==1 and (i%2 != 0 or i == 2) and i !=9:
                 list.append(i)
     return list
